f4df1a5881c5f3152119f93882b9deaf3c322856
andbroby <and.broby@gmail.com>
2018-01-31 13:36:45 +0100

    stuff

--- cfoinsights/settings.py
+++ cfoinsights/settings.py
@@ -197,6 +197,18 @@ CMS_PERMISSION = True
 CMS_PLACEHOLDER_CONF = {}
 
 import dj_database_url
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cfoinsights',
+        'USER': 'cfoinsights',
+        'PASSWORD': 'cfoinsights',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
