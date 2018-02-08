3495de81ffc4dca52f1cd230f0cd50fb795979b1
andbroby <and.broby@gmail.com>
2018-02-08 02:38:58 +0100

    linkedin tag

---
+++ landing/static/landing/landing.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.register-button').click(function(e) {
+        $('#tickets').append('<img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=157308&conversionId=210212&fmt=gif" />');
+        return true;
+    })
+});
