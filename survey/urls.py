6de31c30323781b9c49dc537f49fbab60aa0ec68
Andreas Broby <and.broby@gmail.com>
2018-02-28 05:53:14 +0100

    survey

--- survey/urls.py
+++ survey/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.SurveyTemplateView.as_view(), name='survey'),
     url(r'about/$', views.SurveyAboutView.as_view(), name='about'),
     url(r'questions/$', views.SurveyQuestionsView.as_view(), name='questions'),
-    url(r'ajax/submit$', views.survey_ajax_submit, name='questions'),
+    url(r'ajax/submit$', views.survey_ajax_submit, name='submit_response'),
 ]
